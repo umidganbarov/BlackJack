14a598e9b332acc70a89c0ad40ee95dd5cc75855
Umid Ganbarov <uganbarov23846@ada.edu.az>
2026-05-31 02:47:16 +0400

    Create BET.py

---
+++ BET.py
@@ -0,0 +1,12 @@
+class Bet5:
+    
+    def __init__(self):
+        self.bet=5
+    def getBet(self):
+        return self.bet
+    
+class betFull:
+    def __init__(self,balance):
+        self.balance=balance
+    def getBet(self,balance):
+        return self.balance
