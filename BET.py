c87b52cde42aa6abc61c9dd56dc47e29266f0ffd
Umid Ganbarov <uganbarov23846@ada.edu.az>
2026-06-12 03:09:08 +0400

    Refactor BetMartingale class and improve formatting

--- BET.py
+++ BET.py
@@ -2,6 +2,7 @@ class BetX:#                Bet 5
     def getBet(self,X):
         return X
 #percents as experiment
+
 class BetXP:
     def getBet(self,iBalance,X):
         return max(1,int(iBalance*(X/100)))
@@ -10,9 +11,8 @@ class BetFull:#             Bet your all balance
         self.balance=balance
     def getBet(self):
         return self.balance
-class BetMartingale:
-    
-    def getBet(self,lastHandWon,lastHandBet):#lasthandwon is trye or false
+class BetMartingale:  
+    def getBet(self,lastHandWon,  lastHandBet): #lasthandwon is trye or false
         #? If false bet2x, if none or True bet 1
         #? It must be none in starting shoe 
         if lastHandWon in [True,None]:
@@ -22,3 +22,14 @@ class BetMartingale:
         elif lastHandWon==False:
             return lastHandBet*2
     
+
+            
+
+
+
+
+
+
+
+
+#!  ADD COUNTING PLAYER DEALER IN MAIN HARA QOYASSAN ONA BAX
