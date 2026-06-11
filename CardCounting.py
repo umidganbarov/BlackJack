c6eb9e5da17629c309bb65f686f34154c3ef0a46
Umid Ganbarov <uganbarov23846@ada.edu.az>
2026-06-12 03:09:50 +0400

    Update CardCounting.py

--- CardCounting.py
+++ CardCounting.py
@@ -3,6 +3,7 @@ class CounterHiLo:
         if card in [2,3,4,5,6]:return 1
         elif card in [7,8,9]:return 0
         else: return -1
+            
 class Dont():
     def count(self,player,dealer):
         return 0
